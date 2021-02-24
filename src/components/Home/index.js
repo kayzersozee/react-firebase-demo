d83b4c72eace0bda5a63bc36c95b4a8f05677e98
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-24 11:36:43 +0100

    accountpage and homepage are protected from signout users

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function index() {
-  return <div></div>
-}
+import { withAuthorization } from "../Session";
 
-export default index
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
+  </div>
+);
+
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
