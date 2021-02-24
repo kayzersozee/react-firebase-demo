9fdca76199c5c7779d7e1b8db1ee12a0808f19e4
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-24 10:38:23 +0100

    added provider to withauthentication and moved it from app component

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
+import AuthUserContext from "./context";
+import withAuthentication from "./withAuthentication";
 
-function index() {
-  return <div></div>
-}
-
-export default index
+export { AuthUserContext, withAuthentication };
